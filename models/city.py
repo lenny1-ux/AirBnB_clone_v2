ba152d28726649b55399c132c9055542a2ddf2b6
Metrinenaliaka <metrinenam@gmail.com>
2024-01-15 01:49:14 -0800

    updated the models

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the city class"""
+from os import getenv
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -15,7 +16,11 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="cities")
+    else:
+        state_id = ""
+        name = ""
