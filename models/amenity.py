45902c8c7e05d390714d9a88590b0991ceb16983
Metrinenaliaka <128808519+Metrinenaliaka@users.noreply.github.com>
2024-01-16 09:37:51 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -6,14 +6,19 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
+from sqlalchemy.orm import backref
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
     '''
         Implementation for the Amenities.
     '''
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place',
-                                   secondary=place_amenity,
-                                   back_populates="amenities")
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('PlaceAmenity',
+                                       backref='amenities',
+                                       cascade='delete')
+    else:
+        name = ''
